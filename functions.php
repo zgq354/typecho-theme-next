703dbcc9f80ac85d7decf39d277b82b8e570505a
qing <1017967407@qq.com>
2016-11-10 09:45:47 +0800

    fixed #4

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function getGravatar($email, $s = 40, $d = 'mm', $g = 'g') {
 
     $hash = md5($email);
 
-    $avatar = "http://gravatar.duoshuo.com/avatar/$hash?s=$s&d=$d&r=$g";
+    $avatar = "//cdn.v2ex.com/gravatar/$hash?s=$s&d=$d&r=$g";
 
     return $avatar;
 
